9c3092e941646140b3f2c62caff835f8b95adc2e
vedaka1 <vladik.kuleshov.2003@mail.ru>
2023-09-23 21:04:31 +0300

    fixes

--- main.py
+++ main.py
@@ -6,17 +6,20 @@ from resources.chatgpt import ChatGPT
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode="HTML")
 dp = Dispatcher()
-echo_mode = True
+echo_mode = False
 chat = ChatGPT()
+
 
 async def main():
     await dp.start_polling(bot)
+
 
 @dp.message(filters.Command("start-chat"))
 async def cmd_start(message: types.Message):
     global echo_mode
     echo_mode = True
     await message.answer("ChatGPT mode enabled")
+
 
 @dp.message(filters.Command("end-chat"))
 async def cmd_start(message: types.Message):
@@ -33,6 +36,7 @@ async def cmd_start(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
     await message.answer("The Beginning After the End\nLight novel", reply_markup=keyboard)
 
+
 @dp.message(F.text.lower() == "последние главы")
 async def get_chapters(message: types.Message):
     chapters = parse.get_last_chapters('https://readlightnovel.app/the-beginning-after-the-end-535558')
@@ -46,6 +50,7 @@ async def get_chapters(message: types.Message):
     print(text)
     await message.answer(text, reply_markup=keyboard, parse_mode="HTML")
 
+
 @dp.message(F.text.contains("CH"))
 async def get_chapter_translate(message: types.Message):
     number = message.text[3:]
@@ -54,23 +59,27 @@ async def get_chapter_translate(message: types.Message):
         ]
     keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
     try:
-        if not os.path.exists(f'C:/Projects/Python/aiogram-bot/translated/{number}.txt'): 
+        if not os.path.exists(f'translated/{number}.txt'):
             await message.answer("Подождите, идет перевод")
             parse.get_chapter_text(f'https://readlightnovel.app/the-beginning-after-the-end-535558/chapter-{number}', number)
-        file = types.FSInputFile(f'C:/Projects/Python/aiogram-bot/translated/{number}.txt')
+        file = types.FSInputFile(f'translated/{number}.txt')
         await message.answer_document(file, reply_markup=keyboard)
     except:
         await message.answer("Возникла ошибка при переводе, попробуйте позже", reply_markup=keyboard)
         print(traceback.format_exc())
 
+
 @dp.message(lambda m: echo_mode)
 async def echo(message: types.Message):
     chat.append_message(message.text)
-    try:
+    msg = await bot.send_message(text = f"<i>Waiting</i> \U0001F551", chat_id= message.chat.id)
+    try: 
         response = chat.response_completion()
-        await message.answer(f"{html.quote(response)}")
+        await bot.edit_message_text(text = f"{html.quote(response)}", message_id= msg.message_id, chat_id= msg.chat.id)
     except:
+        await bot.edit_message_text(text = "\U00002757 <i>Error, please try again later</i>", message_id= msg.message_id, chat_id= msg.chat.id)
         print(traceback.format_exc())   
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
